5285911ac0bb2824b746bec9655681d8ecb1b107
ameyakore <kore.ameya@gmail.com>
2021-12-28 20:02:01 +0530

    Initial Commit

---
+++ calc_v3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  calc_v3
+//
+//  Created by Ameya Kore on 28/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
